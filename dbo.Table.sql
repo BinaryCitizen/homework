761f4fb6f8e4c7e948e874f2ccc0e75df03cd3ac
Binary <67679202+BinaryCitizen@users.noreply.github.com>
2021-11-16 10:58:05 +0300

    Add files via upload

---
+++ dbo.Table.sql
@@ -0,0 +1,17 @@
+﻿CREATE TABLE [dbo].[Users]
+(
+	[IdUsers] INT NOT NULL PRIMARY KEY, 
+    [Login] NCHAR(10) NULL, 
+    [Password] NCHAR(10) NULL, 
+    [Phone] INT NULL 
+)
+
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'login',
+    @level0type = N'SCHEMA',
+    @level0name = N'dbo',
+    @level1type = N'TABLE',
+    @level1name = N'Table',
+    @level2type = N'COLUMN',
+    @level2name = N'IdUsers'
