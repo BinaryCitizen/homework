c8ec54787e8c0a4e39b4389d0ca2c0bfc5be3bdc
Binary <67679202+BinaryCitizen@users.noreply.github.com>
2021-11-24 20:02:28 +0300

    Add files via upload

---
+++ DyakonenkoFormsApp1/DyakonenkoFormsApp1/Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿
+namespace DyakonenkoFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnInsert = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.textFIO = new System.Windows.Forms.TextBox();
+            this.textDolznost = new System.Windows.Forms.TextBox();
+            this.textZarplata = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(166, 23);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ФИО Сотрудника";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(13, 83);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(110, 23);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Должность";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(13, 159);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(87, 23);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Зарплата";
+            // 
+            // btnInsert
+            // 
+            this.btnInsert.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnInsert.Location = new System.Drawing.Point(95, 233);
+            this.btnInsert.Name = "btnInsert";
+            this.btnInsert.Size = new System.Drawing.Size(162, 55);
+            this.btnInsert.TabIndex = 3;
+            this.btnInsert.Text = "Вставить запись";
+            this.btnInsert.UseVisualStyleBackColor = true;
+            this.btnInsert.Click += new System.EventHandler(this.btnInsert_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnExit.Location = new System.Drawing.Point(307, 233);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(162, 55);
+            this.btnExit.TabIndex = 4;
+            this.btnExit.Text = "Выход";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // textFIO
+            // 
+            this.textFIO.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textFIO.Location = new System.Drawing.Point(204, 10);
+            this.textFIO.Name = "textFIO";
+            this.textFIO.Size = new System.Drawing.Size(248, 32);
+            this.textFIO.TabIndex = 5;
+            // 
+            // textDolznost
+            // 
+            this.textDolznost.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textDolznost.Location = new System.Drawing.Point(204, 81);
+            this.textDolznost.Name = "textDolznost";
+            this.textDolznost.Size = new System.Drawing.Size(248, 32);
+            this.textDolznost.TabIndex = 6;
+            // 
+            // textZarplata
+            // 
+            this.textZarplata.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textZarplata.Location = new System.Drawing.Point(204, 157);
+            this.textZarplata.Name = "textZarplata";
+            this.textZarplata.Size = new System.Drawing.Size(248, 32);
+            this.textZarplata.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(574, 315);
+            this.Controls.Add(this.textZarplata);
+            this.Controls.Add(this.textDolznost);
+            this.Controls.Add(this.textFIO);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnInsert);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnInsert;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.TextBox textFIO;
+        private System.Windows.Forms.TextBox textDolznost;
+        private System.Windows.Forms.TextBox textZarplata;
+    }
+}
+
---
+++ DyakonenkoFormsApp1/DyakonenkoFormsApp1/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.Sql;
+using System.Data.SqlClient;
+
+namespace DyakonenkoFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private SqlConnection sqlconnection = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            sqlconnection = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\lirex\source\repos\DyakonenkoFormsApp1\DyakonenkoFormsApp1\Database1.mdf;Integrated Security=True");
+            sqlconnection.Open();
+            if (sqlconnection.State == ConnectionState.Open)
+            {
+                MessageBox.Show("Подключение установленно");
+            }
+        }
+
+        private void btnInsert_Click(object sender, EventArgs e)
+        {
+            sqlconnection
+        }
+    }
+}
